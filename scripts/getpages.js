ab2c80e071727b2033b1eab6e0ab53f8ea48530f
salping <amberisamab@gmail.com>
2025-02-12 12:41:34 +1300

    made whole div clickable for blog posts

--- scripts/getpages.js
+++ scripts/getpages.js
@@ -6,19 +6,21 @@ fetch(`../logs/pages.json`).then((response) => {
         pagedata = data;
         for (e in pagedata["pages"]) {
             console.log(e)
+            let a = document.createElement("a");
             let li = document.createElement("div");
             li.className = "blogpage";
-            let element = document.createElement("a")
+            let element = document.createElement("span")
             element.className = "titlea"
             element.innerText = `${e}`;
             let element2 = document.createElement("span");
             element2.className = "descspan"
             element2.innerText = pagedata["pages"][e];
-            element.href = `view/index.html?page=${e}`
+            a.href = `view/index.html?page=${e}`
             li.appendChild(element);
             li.appendChild(document.createElement("br"));
             li.appendChild(element2);
-            pages.appendChild(li)
+            a.appendChild(li)
+            pages.appendChild(a)
         }
     })
 });
