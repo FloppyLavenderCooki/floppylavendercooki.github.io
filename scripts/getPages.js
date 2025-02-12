85b6ab82353457b1c1e4a4f162df1ee4cef30aa9
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-02-13 09:27:03 +1300

    Added sort select

--- scripts/getPages.js
+++ scripts/getPages.js
@@ -1,55 +1,75 @@
 // let pageData = {};
 // let description = document.getElementById("description");
 let pages = document.getElementById("pages");
+const sort = document.getElementById("sort");
+sort.onchange = () => {
+    localStorage.setItem("sort", sort.value);
+    pages.innerHTML = "";
+    fetchPageData();
+}
+if (localStorage.getItem("sort")) {
+    sort.value = localStorage.getItem("sort");
+}
+
+function makePage(e, pageData) {
+    let a = document.createElement("a");
+    a.href = `view/index.html?page=${e}`;
+    a.id = `page-${e}`;
+
+    let div = document.createElement("div");
+    div.className = "blog-page";
+
+    let title = document.createElement("span");
+    title.className = "title-a";
+    title.innerText = `${e}`;
+
+    let descSpan = document.createElement("span");
+    descSpan.className = "desc-span";
+    descSpan.innerText = pageData["pages"][e];
+
+    div.append(title, document.createElement("br"), descSpan);
+    a.appendChild(div);
+    pages.appendChild(a);
+}
 
 async function fetchPageData() {
     try {
         const response = await fetch(`../logs/pages.json`);
         const pageData = await response.json();
-        let lastModifiedArr = {};
 
-        for (let e in pageData["pages"]) {
-            try {
-                const mdResponse = await fetch(`../logs/${e}.md`);
-                const lastModified = mdResponse.headers.get('Last-Modified');
+        switch (sort.value) {
+            case "latest":
+                let lastModifiedArr = {};
 
-                if (lastModified) {
-                    // console.log('Last Modified:', lastModified);
-                    lastModifiedArr[e] = lastModified;
-                } else {
-                    console.log('No Last-Modified header found');
+                for (let e in pageData["pages"]) {
+                    try {
+                        const mdResponse = await fetch(`../logs/${e}.md`);
+                        const lastModified = mdResponse.headers.get('Last-Modified');
+
+                        if (lastModified) {
+                            // console.log('Last Modified:', lastModified);
+                            lastModifiedArr[e] = lastModified;
+                        } else {
+                            console.log('No Last-Modified header found');
+                        }
+                    } catch (err) {
+                        console.log(`Error fetching last modified for ${e}:`, err);
+                    }
                 }
-            } catch (err) {
-                console.log(`Error fetching last modified for ${e}:`, err);
-            }
 
-            let a = document.createElement("a");
-            a.href = `view/index.html?page=${e}`;
-            a.id = `page-${e}`;
+                lastModifiedArr = Object.fromEntries(Object.entries(lastModifiedArr)
+                    .sort((a, b) => new Date(b[1]) - new Date(a[1])));
 
-            let div = document.createElement("div");
-            div.className = "blog-page";
-
-            let title = document.createElement("span");
-            title.className = "title-a";
-            title.innerText = `${e}`;
-
-            let descSpan = document.createElement("span");
-            descSpan.className = "desc-span";
-            descSpan.innerText = pageData["pages"][e];
-
-            div.append(title, document.createElement("br"), descSpan);
-            a.appendChild(div);
-            pages.appendChild(a);
+                for (let e in lastModifiedArr) {
+                    makePage(e, pageData);
+                }
+                break;
+            default:
+                for (let e in pageData["pages"]) {
+                    makePage(e, pageData);
+                }
+                break;
         }
-
-        lastModifiedArr = Object.fromEntries(Object.entries(lastModifiedArr)
-            .sort((a, b) => new Date(b[1]) - new Date(a[1])));
-        for (let e in lastModifiedArr) {
-            let el = document.getElementById(`page-${e}`);
-            el.parentElement.appendChild(document.getElementById(`page-${e}`));
-        }
-
     } catch (error) {
         console.log("Error fetching pages data:", error);
     }
