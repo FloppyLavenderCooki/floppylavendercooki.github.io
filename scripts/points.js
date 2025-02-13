43d56b2ae44e95f2b95024dbe4967fe63e7ff90c
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-02-13 13:12:22 +1300

    Fixed points parse and added drag to move

--- scripts/points.js
+++ scripts/points.js
@@ -4,31 +4,67 @@ let ctx = canvas.getContext("2d");
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.width;
 
+let offsetX = 4;
+let offsetY = 4;
+let points;
+let drawWidth;
+let drawHeight;
+
+function drawPoints() {
+    ctx.strokeStyle = "#ff00ff";
+    ctx.lineWidth = drawWidth/16;
+
+    ctx.beginPath();
+    ctx.moveTo(points[0].x * drawWidth + offsetX, points[0].y * drawHeight + offsetY);
+    for (let i = 1; i < points.length; i++) {
+        ctx.lineTo(points[i].x * drawWidth + offsetX, points[i].y * drawHeight + offsetY);
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+}
+
 fetch("../logs/points.txt").then((data) => {
     data.text().then((data) => {
-        let points = JSON.parse(
-            data.replace(/\((\d+),\s*(\d+)\)/g, '[$1, $2]')
+        points = JSON.parse(
+            data.replaceAll("(","[")
+                .replaceAll(")","]")
         ).map(([x, y]) => ({ x, y }));
         // console.log(points);
 
-        let drawWidth = canvas.width/Math.max(...points.map(point => point.x));
-        let drawHeight = canvas.height/Math.max(...points.map(point => point.x));
+        drawWidth = canvas.width/Math.max(...points.map(point => point.x)) - offsetX;
+        drawHeight = canvas.height/Math.max(...points.map(point => point.x)) - offsetY;
 
-        // ctx.fillStyle = "#ffa500";
-        ctx.strokeStyle = "#ff00ff";
-        ctx.lineWidth = drawWidth/8;
-
-        ctx.beginPath();
-        ctx.moveTo(points[0].x * drawWidth, canvas.height - points[0].y * drawHeight);
-        for (let i = 1; i < points.length; i++) {
-            ctx.lineTo(points[i].x * drawWidth, canvas.height - points[i].y * drawHeight);
-        }
-
-        ctx.save();
-        ctx.clip();
-        ctx.lineWidth *= 2;
-        // ctx.fill();
-        ctx.stroke();
-        ctx.restore();
+        drawPoints();
     })
-})
+});
+
+let moving = false;
+
+canvas.onpointermove = (e) => {
+    if (moving) {
+        offsetX += e.movementX;
+        offsetY += e.movementY;
+
+        drawPoints();
+    }
+}
+
+canvas.onpointerdown = (e) => {
+    moving = true;
+    canvas.setPointerCapture(e.pointerId);
+}
+
+canvas.onpointerup = (e) => {
+    moving = false;
+    canvas.releasePointerCapture(e.pointerId);
+}
+
+window.onkeyup = (e) => {
+    if (e.code === "KeyR") {
+        e.preventDefault();
+        offsetX = 4;
+        offsetY = 4;
+        drawPoints();
+    }
+}
