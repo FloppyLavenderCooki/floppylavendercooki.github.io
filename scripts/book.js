0c742ae95210badbcff63deb6b6bd65e90ec8dc3
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-02-14 19:24:47 +1300

    Created book_gen folder

--- scripts/book.js
+++ scripts/book.js
@@ -1,19 +1,19 @@
-fetch("../logs/book1.txt").then((response) => response.text()).then((data) => {
+fetch("../logs/book_gen/book1.txt").then((response) => response.text()).then((data) => {
     document.getElementById("name").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())] + " ";
-    fetch("../logs/book2.txt").then((response) => response.text()).then((data) => {
+    fetch("../logs/book_gen/book2.txt").then((response) => response.text()).then((data) => {
         document.getElementById("name").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())] + " ";
-        fetch("../logs/book3.txt").then((response) => response.text()).then((data) => {
+        fetch("../logs/book_gen/book3.txt").then((response) => response.text()).then((data) => {
             document.getElementById("name").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())];
         })
     })
 })
 
 document.getElementById("author").textContent = "by "
-fetch("../logs/name1.txt").then((response) => response.text()).then((data) => {
+fetch("../logs/book_gen/name1.txt").then((response) => response.text()).then((data) => {
     document.getElementById("author").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())] + ". ";
-    fetch("../logs/name2.txt").then((response) => response.text()).then((data) => {
+    fetch("../logs/book_gen/name2.txt").then((response) => response.text()).then((data) => {
         document.getElementById("author").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())];
-        fetch("../logs/name3.txt").then((response) => response.text()).then((data) => {
+        fetch("../logs/book_gen/name3.txt").then((response) => response.text()).then((data) => {
             document.getElementById("author").textContent += data.split("\r\n")[Math.floor(data.split("\r\n").length * Math.random())];
         })
     })
