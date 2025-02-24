a04255dc606f331ccc09ad9a7b22655c49fb1516
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2025-02-24 16:21:03 +1300

    3D Rotate and Image Loading

--- scripts/parser.js
+++ scripts/parser.js
@@ -12,7 +12,16 @@ const page = document.getElementById("page");
 
 function parse(data) {
     let sectionHTML = marked.parse(data);
-    page.innerHTML += "<br><br>" + sectionHTML;
+
+    let sectionHTMLTest = document.createElement("div");
+    sectionHTMLTest.innerHTML = sectionHTML
+
+    if (sectionHTMLTest.getElementsByTagName("img").length > 0) {
+        page.innerHTML += "<div style='visibility: hidden'><br><br>" + sectionHTML + "</div>";
+    } else {
+        page.innerHTML += "<br><br>" + sectionHTML;
+    }
+
     page.getElementsByTagName("h1")[0].id = "title";
     page.getElementsByTagName("h1")[0].className = "rubik-800";
     document.title = "Team 2 - " + urlParams.get("page");
@@ -24,5 +33,26 @@ function parse(data) {
         script.remove();
     }
 
-    document.getElementById("loading").remove();
+    if (sectionHTMLTest.getElementsByTagName("img").length > 0) {
+        let images = document.images,
+            len = images.length,
+            counter = 0;
+
+        [].forEach.call( images, function( img ) {
+            if(img.complete)
+                incrementCounter();
+            else
+                img.addEventListener( 'load', incrementCounter, false );
+        } );
+
+        function incrementCounter() {
+            counter++;
+            if ( counter === len ) {
+                document.getElementById("loading").remove();
+                page.innerHTML = page.innerHTML.replaceAll('<div style="visibility: hidden">',"<div>");
+            }
+        }
+    } else {
+        document.getElementById("loading").remove();
+    }
 }
